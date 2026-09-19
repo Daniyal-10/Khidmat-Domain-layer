9f674db6b671492040544e7434ffc07bae759940
Daniyal-10 <daniyaalkhan077@gmail.com>
2026-09-19 18:29:34 +0530

    chore(ontology): remove Stage 5 reconciliation scratch artifacts

--- khidmat-knowledge/scratch/list_keys.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('scratch/gtr_data.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-# Let's map out the evidence for all 47 to make a decision
-keys = data.keys()
-for k in keys:
-    print(k, data[k]['r1_status'])
